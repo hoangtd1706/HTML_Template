6b474987f72951cbcbc42630fd90899fad5261fd
hoangtd1706 <trananhhoang6699@gmail.com>
2021-07-03 00:07:20 +0700

    commit 4

--- BCI/assets/js/script.js
+++ BCI/assets/js/script.js
@@ -3,10 +3,42 @@ $(document).ready(function () {
         $(this).toggleClass('active');
         if ($(this).hasClass('active')) {
             $('.menu').addClass('active');
+            $('.close_menu').addClass('active');
             $('.menu').css('right', 0);
         } else {
             $('.menu').removeClass('active');
+            $('.close_menu').removeClass('active');
             $('.menu').css('right', -440);
         }
+    })
+
+    $('.close_menu').on('click', function () {
+        $('.btn_menu').removeClass('active');
+        $('.menu').removeClass('active');
+        $('.close_menu').removeClass('active');
+        $('.menu').css('right', -440);
+    })
+    $(window).scroll(function () {
+        var aTop = $(window).scrollTop();
+        if (aTop > 244) {
+            $('.site39_header_col0').addClass('is-menu');
+        } else {
+            $('.site39_header_col0').removeClass('is-menu');
+        }
+    })
+
+    $('.to_top').on('click', function () {
+        $("html, body").stop().animate({ scrollTop: 0 }, 500, 'swing');
+    })
+    $(window).scroll(function () {
+        let toTop = $(this).scrollTop();
+        console.log(toTop);
+        (toTop > 150) ? gsap.to("#icon_right_1", { duration: 1, y: 200 }) : null;
+        (toTop > 598) ? gsap.to("#icon_right_2", { duration: 1, x: -100 }) : null;
+        (toTop > 869) ? gsap.to("#icon_left", { duration: 1, x: 100 }) : null;
+
+        (toTop < 150) ? gsap.to("#icon_right_1", { duration: 1, y: -200 }) : null;
+        (toTop < 598) ? gsap.to("#icon_right_2", { duration: 1, x: 100 }) : null;
+        (toTop < 869) ? gsap.to("#icon_left", { duration: 1, x: -100 }) : null;
     })
 })
