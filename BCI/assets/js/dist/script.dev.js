303083d88d2a8bc1d2572db9c048f7008e997fc7
hoang <hoangtd1706@gmail.com>
2021-07-03 11:54:46 +0700

    commit 5

---
+++ BCI/assets/js/dist/script.dev.js
@@ -0,0 +1,83 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.btn_menu').on('click', function () {
+    $(this).toggleClass('active');
+
+    if ($(this).hasClass('active')) {
+      $('.menu').addClass('active');
+      $('.close_menu').addClass('active'); //$('.menu').css('right', 0);
+    } else {
+      $('.menu').removeClass('active');
+      $('.close_menu').removeClass('active'); //$('.menu').css('right', -440);
+    }
+  });
+  $('.close_menu').on('click', function () {
+    $('.btn_menu').removeClass('active');
+    $('.menu').removeClass('active');
+    $('.close_menu').removeClass('active');
+    $('.menu').css('right', -440);
+  });
+  /* $(window).scroll(function () {
+      var aTop = $(window).scrollTop();
+      if (aTop > 244) {
+          $('.site39_header_col0').addClass('is-menu');
+      } else {
+          $('.site39_header_col0').removeClass('is-menu');
+      }
+  }) */
+
+  $('.to_top').on('click', function () {
+    $("html, body").stop().animate({
+      scrollTop: 0
+    }, 500, 'swing');
+  });
+  $(window).scroll(function () {
+    var page_height = $('body').height();
+    var window_height = $(window).height();
+    var toTop = $(this).scrollTop();
+    console.log(toTop);
+    toTop > 150 ? gsap.to("#icon_right_1", {
+      duration: 1,
+      y: 200
+    }) : gsap.to("#icon_right_1", {
+      duration: 1,
+      y: -200
+    });
+    toTop > 598 ? gsap.to("#icon_right_2", {
+      duration: 1,
+      x: -100
+    }) : gsap.to("#icon_right_2", {
+      duration: 1,
+      x: 100
+    });
+    toTop > 869 ? gsap.to("#icon_left", {
+      duration: 1,
+      x: 100
+    }) : gsap.to("#icon_left", {
+      duration: 1,
+      x: -100
+    });
+    toTop > 1800 && toTop < 3027 ? gsap.to("#icon_1", {
+      duration: 1,
+      x: -100
+    }) : gsap.to("#icon_1", {
+      duration: 1,
+      x: 100
+    });
+    toTop > 3020 && toTop < 4000 ? gsap.to("#icon_2", {
+      duration: 1,
+      y: -100
+    }) : gsap.to("#icon_2", {
+      duration: 1,
+      y: 100
+    });
+    toTop > 3060 ? gsap.to("#btn_social", {
+      duration: 1,
+      y: -230
+    }) : gsap.to("#btn_social", {
+      duration: 1,
+      y: 0
+    });
+  });
+});
--- BCI/assets/js/script.js
+++ BCI/assets/js/script.js
@@ -4,11 +4,12 @@ $(document).ready(function () {
         if ($(this).hasClass('active')) {
             $('.menu').addClass('active');
             $('.close_menu').addClass('active');
-            $('.menu').css('right', 0);
+            //$('.menu').css('right', 0);
+
         } else {
             $('.menu').removeClass('active');
             $('.close_menu').removeClass('active');
-            $('.menu').css('right', -440);
+            //$('.menu').css('right', -440);
         }
     })
 
@@ -18,27 +19,71 @@ $(document).ready(function () {
         $('.close_menu').removeClass('active');
         $('.menu').css('right', -440);
     })
-    $(window).scroll(function () {
+    /* $(window).scroll(function () {
         var aTop = $(window).scrollTop();
         if (aTop > 244) {
             $('.site39_header_col0').addClass('is-menu');
         } else {
             $('.site39_header_col0').removeClass('is-menu');
         }
-    })
+    }) */
 
     $('.to_top').on('click', function () {
-        $("html, body").stop().animate({ scrollTop: 0 }, 500, 'swing');
+        $("html, body").stop().animate({
+            scrollTop: 0
+        }, 500, 'swing');
     })
     $(window).scroll(function () {
+        let page_height = $('body').height();
+        let window_height = $(window).height();
         let toTop = $(this).scrollTop();
         console.log(toTop);
-        (toTop > 150) ? gsap.to("#icon_right_1", { duration: 1, y: 200 }) : null;
-        (toTop > 598) ? gsap.to("#icon_right_2", { duration: 1, x: -100 }) : null;
-        (toTop > 869) ? gsap.to("#icon_left", { duration: 1, x: 100 }) : null;
+        (toTop > 150) ? gsap.to("#icon_right_1", {
+            duration: 1,
+            y: 200
+        }): gsap.to("#icon_right_1", {
+            duration: 1,
+            y: -200
+        });
 
-        (toTop < 150) ? gsap.to("#icon_right_1", { duration: 1, y: -200 }) : null;
-        (toTop < 598) ? gsap.to("#icon_right_2", { duration: 1, x: 100 }) : null;
-        (toTop < 869) ? gsap.to("#icon_left", { duration: 1, x: -100 }) : null;
+        (toTop > 598) ? gsap.to("#icon_right_2", {
+            duration: 1,
+            x: -100
+        }): gsap.to("#icon_right_2", {
+            duration: 1,
+            x: 100
+        });
+
+        (toTop > 869) ? gsap.to("#icon_left", {
+            duration: 1,
+            x: 100
+        }): gsap.to("#icon_left", {
+            duration: 1,
+            x: -100
+        });
+
+        (toTop > 1800 && toTop < 3027) ? gsap.to("#icon_1", {
+            duration: 1,
+            x: -100
+        }): gsap.to("#icon_1", {
+            duration: 1,
+            x: 100
+        });
+
+        (toTop > 3020 && toTop < 4000) ? gsap.to("#icon_2", {
+            duration: 1,
+            y: -100
+        }): gsap.to("#icon_2", {
+            duration: 1,
+            y: 100
+        });
+
+        (toTop > 3060) ? gsap.to("#btn_social", {
+            duration: 1,
+            y: -230
+        }): gsap.to("#btn_social", {
+            duration: 1,
+            y: 0
+        })
     })
 })
