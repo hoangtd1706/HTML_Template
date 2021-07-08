628b5f085eaf89bb1507da2cf1ae3ad9fecb6db0
hoang <hoangtd1706@gmail.com>
2021-07-08 16:48:34 +0700

    commit 12

--- BCI/assets/js/huanluyen.js
+++ BCI/assets/js/huanluyen.js
@@ -38,4 +38,23 @@ $(document).ready(function () {
             }
         }]
     });
+    let slide4 = $('.slide_course').slick({
+        "slidesToShow": 3,
+        "slidesToScroll": 1,
+        "prevArrow": $(".prev_1"),
+        "nextArrow": $(".next_1"),
+        "responsive": [{
+            "breakpoint": 1200,
+            "settings": {
+                "slidesToShow": 2,
+                "slidesToScroll": 1
+            }
+        }, {
+            "breakpoint": 600,
+            "settings": {
+                "slidesToShow": 1,
+                "slidesToScroll": 1
+            }
+        }]
+    });
 });
