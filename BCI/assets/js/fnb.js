5c80efdab21b91303a7091b99771a46957d04fdf
hoang <hoangtd1706@gmail.com>
2021-07-15 15:03:38 +0700

    commit 14

---
+++ BCI/assets/js/fnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    let slide1 = $('.slide').slick({
+        "slidesToShow": 1,
+        "slidesToScroll": 1,
+        "prevArrow": $(".btn_prev"),
+        "nextArrow": $(".btn_next"),
+        "fade": true,
+        "autoplay": true,
+    })
+
+
+    $('.slide').on('afterChange', function (event, slick, currentSlide) {
+        console.log(currentSlide)
+        if (currentSlide > 0) {
+            $('.btn_prev').removeClass('hidden');
+        } else {
+            $('.btn_prev').addClass('hidden');
+        }
+
+        if (currentSlide === 0) {
+            $('.btn_next').removeClass('hidden');
+        } else {
+            $('.btn_next').addClass('hidden');
+        }
+    })
+})
